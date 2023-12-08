f67e266e31078da8e6944c51aa5e85b1a4f281d6
Surush Qalandarsho <qalandarshosurush@gmail.com>
2023-12-08 03:05:08 -0800

    first commit

---
+++ Data.js
@@ -0,0 +1,68 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "labtob",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download (1).jpeg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "labtob",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download (2).jpeg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "labtob",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download (3).jpeg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "labtob",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download (1).jpeg",
+    },
+    {
+      id: "thiecbawdjksadjk",
+      name: "labtob",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download (3).jpeg",
+    },
+    {
+      id: "iuertrywebncdjksadjk",
+      name: "labtob",
+      price: 200,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "download.jpg",
+    },
+  
+  
+  
+    {
+      id: "oiopijmjkhuihb",
+      name: "labtob",
+      price: 120,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images.jpg",
+    },
+    {
+      id: "oiopijewyiohbjhib",
+      name: "labtob",
+      price: 35,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images (1).jpg",
+    },
+    {
+      id: "rtytytuyuytyytbvncv",
+      name: "labtob",
+      price: 350,
+      desc: "Download the perfect laptop computer pictures. Find ",
+      img: "images (1).jpg",
+    },
+  ];
